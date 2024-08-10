215150961a8a21c9dcd565443b2bed2c5b3be1e9
Mykola Maksymiv <kolaymaksimiv@gmail.com>
2024-08-10 16:16:52 +0300

    Add project files.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Stopwatch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            for (int i = 0; i < 2; i++)
+            {
+                stopwatch.Start();
+
+                Thread.Sleep(1000);
+
+                stopwatch.Stop();
+
+                Console.WriteLine("Duration:" + stopwatch.GetInterval().ToString());
+                Console.WriteLine("Press Enter to run the stopwatch one more time.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,33 @@
+﻿namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+
+        private bool _isRunning = false;
+
+        public void Start()
+        {
+            if (_isRunning)
+                throw new InvalidOperationException("Stopwatch is already running");
+
+            _startTime = DateTime.Now;
+            _isRunning = true;
+        }
+
+        public void Stop()
+        {
+            if (!_isRunning)
+                throw new InvalidOperationException("Stopwatch is not running");
+
+            _endTime = DateTime.Now;
+            _isRunning = false;
+        }
+
+        public TimeSpan GetInterval()
+        {
+            return _endTime - _startTime;
+        }
+    }
+}
